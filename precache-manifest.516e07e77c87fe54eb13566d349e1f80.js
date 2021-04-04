3fc2374380b81c14c693297d0e8b742f5285322f
Mayur Jain <maxcharge788769@gmail.com>
2021-04-05 00:18:59 +0530

    Updates

---
+++ precache-manifest.516e07e77c87fe54eb13566d349e1f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818dab568713babde2bb22e71186a6af",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "a533a8e6ab96050a4a2d",
+    "url": "/Portfolio/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "cc4b8934a0a51c8858ff",
+    "url": "/Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a533a8e6ab96050a4a2d",
+    "url": "/Portfolio/static/js/2.6dd83244.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.6dd83244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4b8934a0a51c8858ff",
+    "url": "/Portfolio/static/js/main.157e4a8f.chunk.js"
+  },
+  {
+    "revision": "7de168f8bf4f7677eb45",
+    "url": "/Portfolio/static/js/runtime-main.c512249b.js"
+  }
+]);
